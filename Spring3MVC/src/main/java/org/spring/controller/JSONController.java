87d02467dc61479074eabfb7cb817c4bfa3e1e21
nbuddharaju <navatha.buddharaju@assurity.co.nz>
2015-03-23 10:10:52 +1300

    Fixed dependency issues

--- Spring3MVC/src/main/java/org/spring/controller/JSONController.java
+++ Spring3MVC/src/main/java/org/spring/controller/JSONController.java
@@ -6,14 +6,13 @@ import org.spring.dao.CustomerDao;
 import org.spring.model.Customer;
 import org.spring.model.Shop;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 
 @Controller
 @RequestMapping("/kfc/brands")
@@ -35,7 +34,7 @@ public class JSONController {
 	public @ResponseBody  List<Customer> getAll() {
 		
 		ApplicationContext context = new FileSystemXmlApplicationContext(
-				"bean.xml");
+				"classpath:bean.xml");
 		CustomerDao customer = (CustomerDao) context.getBean("customerdao");
 			
 		List<Customer> customers = customer.getAll();
--- Spring3MVC/src/main/java/org/spring/main/SpringBootAndCrudApplication.java
+++ Spring3MVC/src/main/java/org/spring/main/SpringBootAndCrudApplication.java
@@ -1,17 +1,20 @@
 package org.spring.main;
 
+import java.net.URLClassLoader;
+import java.util.Arrays;
+
 import org.spring.dao.CustomerDao;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import sun.tools.jar.Main;
 
 public class SpringBootAndCrudApplication {
 
 	public static void main(String[] args) {
 		
 		ApplicationContext context = new FileSystemXmlApplicationContext(
-				"bean.xml");
+				"src/main/resources/bean.xml");
 		CustomerDao customer = (CustomerDao) context.getBean("customerdao");
 //		// customer.Create("Navatha");
 //		// customer.Delete(3);
@@ -19,6 +22,9 @@ public class SpringBootAndCrudApplication {
 		System.out.println(customer.getAll());
 
 		//SpringApplication.run(SpringBootAndCrudApplication.class, args);
+		
+		
+
 
 	}
 }
